74fc0cf6b01324eec1166ca79219314288b2bdf8
sethifur <sethjohns@mail.weber.edu>
2017-02-13 14:56:53 -0700

    word changes app.js color changes app.css added a text file to explain what I changed and explane what I learned

--- src/App.js
+++ src/App.js
@@ -11,8 +11,9 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Hello World!
         </p>
+	<p>This is the first time I have used Node.js or React</p>
       </div>
     );
   }
